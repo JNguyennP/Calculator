27f96ea8febcd44d94cf58943a349f2f6af20799
JNguyennP <jnguyenpham83@gmail.com>
2024-08-19 22:52:34 +0100

    Defined performing calculations function

--- calculator.py
+++ calculator.py
@@ -7,21 +7,21 @@ def calculator():
 
             num2 = float(input('Enter Second Number: '))
 
-            if operator == '+':
-                total = num1 + num2
-            elif operator == '-':
-                total = num1 - num2
-            elif operator == '*':
-                total = num1 * num2
-            elif operator == '/':
-                if (num1 != 0 and num2 != 0):
-                    total - num1 / num2
-                else:
-                    print('Error: Dividing by 0 Or Dividing 0 From A Number Is Not Allowed.')
-            else: 
-                print('Invalid Operator.')
+            # if operator == '+':
+            #     total = num1 + num2
+            # elif operator == '-':
+            #     total = num1 - num2
+            # elif operator == '*':
+            #     total = num1 * num2
+            # elif operator == '/':
+            #     if (num1 != 0 and num2 != 0):
+            #         total - num1 / num2
+            #     else:
+            #         print('Error: Dividing by 0 Or Dividing 0 From A Number Is Not Allowed.')
+            # else: 
+            #     print('Invalid Operator.')
 
-            print(f'The result is: {total}')
+            # print(f'The result is: {total}')
         
         except ValueError:
             print('Error: Invalid Input. Please Enter A Number.')
@@ -29,5 +29,26 @@ def calculator():
         repeat = input('Do You Want To Perform Another Calculation? Enter Yes or No: ')
         if repeat.lower() != 'yes':
             break
+
+def performing_calculations(num1, num2, operator):
+    if operator == '+':
+        total = num1 + num2
+    elif operator == '-':
+        total = num1 - num2
+    elif operator == '*':
+        total = num1 * num2
+    elif operator == '/':
+        if (num1 != 0 and num2 != 0):
+            total - num1 / num2
+        else:
+            print('Error: Dividing by 0 Or Dividing 0 From A Number Is Not Allowed.')
+    else: 
+        print('Invalid Operator.')
+
+    print(f'The result is: {total}')
+
+
+
+
 
 print(calculator())
