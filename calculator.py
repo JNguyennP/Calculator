5eafd7d711c7134fd4b9dec8a6e2ce8c27500139
JNguyennP <jnguyenpham83@gmail.com>
2024-08-19 22:43:01 +0100

    Created Loop For Multiple Calculations

--- calculator.py
+++ calculator.py
@@ -1,27 +1,33 @@
 def calculator():
-    try: 
-        num1 = float(input('Enter First Number: '))
+    while True:
+        try: 
+            num1 = float(input('Enter First Number: '))
 
-        operator = input('Enter operator (+, -, *, /):')
+            operator = input('Enter operator (+, -, *, /):')
 
-        num2 = float(input('Enter Second Number: '))
+            num2 = float(input('Enter Second Number: '))
 
-        if operator == '+':
-            total = num1 + num2
-        elif operator == '-':
-            total = num1 - num2
-        elif operator == '*':
-            total = num1 * num2
-        elif operator == '/':
-            if (num1 != 0 and num2 != 0):
-                total - num1 / num2
-            else:
-                return 'Error: Dividing by 0 Or Dividing 0 From A Number Is Not Allowed.'
-        else: 
-            return 'Invalid Operator.'
+            if operator == '+':
+                total = num1 + num2
+            elif operator == '-':
+                total = num1 - num2
+            elif operator == '*':
+                total = num1 * num2
+            elif operator == '/':
+                if (num1 != 0 and num2 != 0):
+                    total - num1 / num2
+                else:
+                    print('Error: Dividing by 0 Or Dividing 0 From A Number Is Not Allowed.')
+            else: 
+                print('Invalid Operator.')
 
-        return f'The result is: {total}'
-    except ValueError:
-        return 'Error: Invalid Input. Please Enter A Number.'
+            print(f'The result is: {total}')
+        
+        except ValueError:
+            print('Error: Invalid Input. Please Enter A Number.')
+        
+        repeat = input('Do You Want To Perform Another Calculation? Enter Yes or No: ')
+        if repeat.lower() != 'yes':
+            break
 
 print(calculator())
