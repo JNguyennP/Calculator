9c7f0c4ed6f58ac9e9fa0bea51378466c507ec7c
JNguyennP <jnguyenpham83@gmail.com>
2024-08-19 22:36:35 +0100

    Handling Non-Numeric Values

--- calculator.py
+++ calculator.py
@@ -1,24 +1,27 @@
 def calculator():
-    num1 = float(input('Enter First Number: '))
+    try: 
+        num1 = float(input('Enter First Number: '))
 
-    operator = input('Enter operator (+, -, *, /):')
+        operator = input('Enter operator (+, -, *, /):')
 
-    num2 = float(input('Enter Second Number: '))
+        num2 = float(input('Enter Second Number: '))
 
-    if operator == '+':
-        total = num1 + num2
-    elif operator == '-':
-        total = num1 - num2
-    elif operator == '*':
-        total = num1 * num2
-    elif operator == '/':
-        if (num1 != 0 and num2 != 0):
-            total - num1 / num2
-        else:
-            return 'Error: Dividing by 0 Or Dividing 0 From A Number Is Not Allowed.'
-    else: 
-        return 'Invalid Operator.'
+        if operator == '+':
+            total = num1 + num2
+        elif operator == '-':
+            total = num1 - num2
+        elif operator == '*':
+            total = num1 * num2
+        elif operator == '/':
+            if (num1 != 0 and num2 != 0):
+                total - num1 / num2
+            else:
+                return 'Error: Dividing by 0 Or Dividing 0 From A Number Is Not Allowed.'
+        else: 
+            return 'Invalid Operator.'
 
-    return f'The result is: {total}'
+        return f'The result is: {total}'
+    except ValueError:
+        return 'Error: Invalid Input. Please Enter A Number.'
 
 print(calculator())
